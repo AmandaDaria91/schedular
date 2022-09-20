b18cd3e42d9bd0b918cfc8f5b6ecaa6f0a463427
AmandaDaria91 <34180620+AmandaDaria91@users.noreply.github.com>
2022-09-20 16:26:35 -0700

    first commit

---
+++ Develop/script.js
@@ -0,0 +1,17 @@
+// TODO: WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+
+// TODO: WHEN I scroll down
+// THEN I am presented with time blocks for standard business hours
+
+// TODO: WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+// TODO: WHEN I click into a time block
+// THEN I can enter an event
+
+// TODO: WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+
+// TODO: WHEN I refresh the page
+// THEN the saved events persist 
